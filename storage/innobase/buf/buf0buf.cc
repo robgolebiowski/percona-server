181a0823b089c24323517f58058e27f87b6ae006
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2017-02-27 09:05:55 +0200

    Fix bug 1664280 (Race condition between buffer pool page optimistic access and eviction | InnoDB: Failing assertion: buf_page_in_file(bpage) in buf_page_make_young)
    buf_page_optimistic_get is passed a buffer pool block pointer with
    nothing locked. Thus it is possible, for example, for the pointed
    block to get evicted from the buffer pool and a different data page to
    be read in. This function does not detect this condition correctly:
    first, it checked whether block state is BUF_BLOCK_FILE_PAGE,
    buffer-pinned the block, latched it, and then compared the
    modify_clock value. The modify_clock value mismatch catches the
    condition of block going to the free list and back as a data page, but
    after this caught, the block is unfixed before returning. However,
    when buf_page_init_for_read was called for this same block, it reset
    buf_fix_count to zero in buf_page_init_low, overwriting the effect of
    the previous buf_block_buf_fix_inc, and making the return unfix result
    in UINT32_MAX as the fix count.
    
    Fix by catching the block pointer pointing to evicted and/or re-read
    data page earlier in the function, before the buffer pool fix
    attempt, together with BUF_BLOCK_FILE_PAGE check. This is correct
    because buf_LRU_block_remove_hashed bumps the modify clock while under
    buffer page mutex protection, in the same critical section as
    BUF_BLOCK_FILE_PAGE -> BUF_BLOCK_REMOVE_HASH transition.
    
    Also replace the buf_fix_count reset in buf_page_init_low with a release
    build assert for zero. Fix buf_block_unfix assert to actually catch
    the unsigned underflow, as the previous expression type was promoted
    to a wider type and thus never became zero.
    
    At the same time move all the side effects of buf_page_optimistic_get
    after the final modify_clock check, to avoid needlessly marking wrong
    pages as young or accessed. Also clarify in the comments and asserts
    that buf_block_modify_clock_inc may be called with the buffer page
    locked instead of the  LRU list mutex.
    
    (cherry picked from commit fb0f018bced4c12438855d320c322ec47e1e19ec)

--- storage/innobase/buf/buf0buf.cc
+++ storage/innobase/buf/buf0buf.cc
@@ -4019,7 +4019,9 @@ buf_page_optimistic_get(
 
 	buf_page_mutex_enter(block);
 
-	if (buf_block_get_state(block) != BUF_BLOCK_FILE_PAGE) {
+	if (UNIV_UNLIKELY(block->modify_clock != modify_clock
+			  || (buf_block_get_state(block)
+			      != BUF_BLOCK_FILE_PAGE))) {
 
 		buf_page_mutex_exit(block);
 
@@ -4028,13 +4030,7 @@ buf_page_optimistic_get(
 
 	buf_block_buf_fix_inc(block, file, line);
 
-	access_time = buf_page_is_accessed(&block->page);
-
-	buf_page_set_accessed(&block->page);
-
 	buf_page_mutex_exit(block);
-
-	buf_page_make_young_if_needed(&block->page);
 
 	ut_ad(!ibuf_inside(mtr)
 	      || ibuf_page(block->page.id, block->page.size, NULL));
@@ -4057,13 +4053,13 @@ buf_page_optimistic_get(
 		ut_error; /* RW_SX_LATCH is not implemented yet */
 	}
 
-	if (!success) {
+	if (UNIV_UNLIKELY(!success)) {
 		buf_block_buf_fix_dec(block);
 
 		return(FALSE);
 	}
 
-	if (modify_clock != block->modify_clock) {
+	if (UNIV_UNLIKELY(modify_clock != block->modify_clock)) {
 
 		buf_block_dbg_add_level(block, SYNC_NO_ORDER_CHECK);
 
@@ -4078,6 +4074,18 @@ buf_page_optimistic_get(
 		return(FALSE);
 	}
 
+	buf_page_mutex_enter(block);
+
+	access_time = buf_page_is_accessed(&block->page);
+
+	buf_page_set_accessed(&block->page);
+
+	ut_ad(!block->page.file_page_was_freed);
+
+	buf_page_mutex_exit(block);
+
+	buf_page_make_young_if_needed(&block->page);
+
 	mtr_memo_push(mtr, block, fix_type);
 
 #if defined UNIV_DEBUG || defined UNIV_BUF_DEBUG
@@ -4087,10 +4095,6 @@ buf_page_optimistic_get(
 	ut_a(block->page.buf_fix_count > 0);
 	ut_a(buf_block_get_state(block) == BUF_BLOCK_FILE_PAGE);
 #endif /* UNIV_DEBUG || UNIV_BUF_DEBUG */
-
-	ut_d(buf_page_mutex_enter(block));
-	ut_ad(!block->page.file_page_was_freed);
-	ut_d(buf_page_mutex_exit(block));
 
 	if (!access_time) {
 		/* In the case of a first access, try to apply linear
@@ -4314,7 +4318,7 @@ buf_page_init_low(
 {
 	bpage->flush_type = BUF_FLUSH_LRU;
 	bpage->io_fix = BUF_IO_NONE;
-	bpage->buf_fix_count = 0;
+	ut_a(bpage->buf_fix_count == 0);
 	bpage->freed_page_clock = 0;
 	bpage->access_time = 0;
 	bpage->newest_modification = 0;
